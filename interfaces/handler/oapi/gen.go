c9bf6568462dca247e70ec4e5ad10919c6bd5aa3
Ras96 <asymptote.k.k@gmail.com>
2021-12-07 14:33:23 +0900

    :bug: Fix bind error

--- interfaces/handler/oapi/gen.go
+++ interfaces/handler/oapi/gen.go
@@ -1,5 +1,11 @@
 //go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest --config ./.server.yml ../../../docs/openapi.yml
 //go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest --config ./.types.yml ../../../docs/openapi.yml
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest --config ./.spec.yml ../../../docs/openapi.yml
 
 package oapi
+
+import "github.com/21hack02win/nascalay-backend/model"
+
+// NOTE: UserIdInQueryをuuid.UUID型にするとBindしてくれないためstring型にしてここで詰め替えをする
+func (u UserIdInQuery) Refill() (model.UserId, error) {
+	return model.UserIdFromString(string(u))
+}
--- interfaces/handler/ws.go
+++ interfaces/handler/ws.go
@@ -4,12 +4,17 @@ import (
 	"net/http"
 
 	"github.com/21hack02win/nascalay-backend/interfaces/handler/oapi"
-	"github.com/21hack02win/nascalay-backend/model"
 	"github.com/labstack/echo/v4"
 )
 
 func (h *handler) Ws(c echo.Context, params oapi.WsParams) error {
-	if err := h.stream.ServeWS(c.Response().Writer, c.Request(), model.UserId(params.User)); err != nil {
+	uid, err := params.User.Refill()
+	if err != nil {
+		// Invalid uuid
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	if err := h.stream.ServeWS(c.Response().Writer, c.Request(), uid); err != nil {
 		c.Logger().Error(err)
 		return newEchoHTTPError(err)
 	}
