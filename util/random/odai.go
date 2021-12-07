8fa813c5460a58c8c46f3c12d41585a2c94f4e1d
trasta <trasta298@gmail.com>
2021-12-08 02:07:35 +0900

    🐛 変数名ミス

--- util/random/odai.go
+++ util/random/odai.go
@@ -3,7 +3,7 @@ package random
 import "math/rand"
 
 var (
-	prefix2Data = [...]string{
+	prefixData = [...]string{
 		"ねこの", "異世界", "アイドル", "天然", "真っ黒な", "トラと", "空から", "伝説の", "昭和", "平成", "大盛り",
 		"宇宙の", "古代",
 	}
@@ -15,7 +15,7 @@ var (
 
 //nolint:unused,deadcode
 func odaiExample() string {
-	prefix := prefix2Data[rand.Intn(len(prefix2Data))]
+	prefix := prefixData[rand.Intn(len(prefixData))]
 	suffix := suffixData[rand.Intn(len(suffixData))]
 	return prefix + suffix
 }
