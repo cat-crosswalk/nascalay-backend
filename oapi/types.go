f41f0d72f6c222fae56c145aad7ea3704e743632
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-12-10 04:50:18 +0900

    え，消えたけど？

--- oapi/types.go
+++ oapi/types.go
@@ -64,26 +64,6 @@ const (
 	WsEventSHOWSTART WsEvent = "SHOW_START"
 )
 
-// Defines values for WsNextShowStatus.
-const (
-	WsNextShowStatusAnswer WsNextShowStatus = "answer"
-
-	WsNextShowStatusCanvas WsNextShowStatus = "canvas"
-
-	WsNextShowStatusEnd WsNextShowStatus = "end"
-
-	WsNextShowStatusOdai WsNextShowStatus = "odai"
-)
-
-// ユーザーが描画するキャンバスの分割情報・描画位置
-type Canvas struct {
-	// ボードの座標ID
-	AreaId int `json:"areaId"`
-
-	// ボード名
-	BoardName string `json:"boardName"`
-}
-
 // 新規ルーム作成リクエスト
 type CreateRoomRequest struct {
 	// アバターの種類
@@ -144,21 +124,6 @@ type WsAnswerSendEventBody struct {
 	Answer string `json:"answer"`
 }
 
-// 絵が飛んできて，回答する (サーバー -> ルーム各員)
-type WsAnswerStartEventBody struct {
-	// 画像ID
-	Img string `json:"img"`
-
-	// 制限時間
-	TimeLimit int `json:"timeLimit"`
-}
-
-// 最後の回答を受信する (サーバー -> ルーム全員)
-type WsChangeHostEventBody struct {
-	// ホストのユーザーUUID
-	HostId string `json:"hostId"`
-}
-
 // 絵を送信する (ルームの各員 -> サーバー)
 //
 // -> (DRAWフェーズが終わってなかったら) また，DRAW_START が飛んでくる
@@ -167,41 +132,8 @@ type WsDrawSendEventBody struct {
 	Img string `json:"img"`
 }
 
-// キャンバス情報とお題を送信する (サーバー -> ルーム各員)
-type WsDrawStartEventBody struct {
-	// 全DRAWフェーズ数
-	AllDrawPhaseNum int `json:"allDrawPhaseNum"`
-
-	// ユーザーが描画するキャンバスの分割情報・描画位置
-	Canvas Canvas `json:"canvas"`
-
-	// 現在のDRAWフェーズの番号
-	DrawPhaseNum int `json:"drawPhaseNum"`
-
-	// 画像ID
-	Img string `json:"img"`
-
-	// お題
-	Odai string `json:"odai"`
-
-	// 制限時間
-	TimeLimit int `json:"timeLimit"`
-}
-
 // Websocketイベントのリスト
 type WsEvent string
-
-// ゲームの開始を通知する (サーバー -> ルーム全員)
-type WsGameStartEventBody struct {
-	// お題のサジェスト
-	OdaiHint string `json:"odaiHint"`
-
-	// 制限時間
-	TimeLimit int `json:"timeLimit"`
-}
-
-// 次のWebsocketイベントのリスト
-type WsNextShowStatus string
 
 // お題を送信する (ルームの各員 -> サーバー)
 type WsOdaiSendEventBody struct {
@@ -209,55 +141,10 @@ type WsOdaiSendEventBody struct {
 	Odai string `json:"odai"`
 }
 
-// 部屋に追加のメンバーが来たことを通知する (サーバー -> ルーム全員)
-type WsRoomNewMemberEventBody struct {
-	// ルームの最大収容人数
-	Capacity int `json:"capacity"`
-
-	// ホストのユーザーUUID
-	HostId uuid.UUID `json:"hostId"`
-
-	// 現在ルームにいるメンバーの情報
-	Members []User `json:"members"`
-}
-
 // ゲームのオプションを設定する (ホスト -> サーバー)
 type WsRoomSetOptionEventBody struct {
 	// 何か
 	Something string `json:"something"`
-}
-
-// ゲームの設定を更新する (サーバー -> ルーム全員)
-type WsRoomUpdateOptionEventBody struct {
-	// 何か
-	Something string `json:"something"`
-}
-
-// 最後の回答を受信する (サーバー -> ルーム全員)
-type WsShowAnswerEventBody struct {
-	// 回答
-	Answer string `json:"answer"`
-
-	// 次のWebsocketイベントのリスト
-	Next WsNextShowStatus `json:"next"`
-}
-
-// 次のキャンバスを受信する (サーバー -> ルーム全員)
-type WsShowCanvasEventBody struct {
-	// 画像ID
-	Img string `json:"img"`
-
-	// 次のWebsocketイベントのリスト
-	Next WsNextShowStatus `json:"next"`
-}
-
-// 最初のお題を受信する (サーバー -> ルーム全員)
-type WsShowOdaiEventBody struct {
-	// 次のWebsocketイベントのリスト
-	Next WsNextShowStatus `json:"next"`
-
-	// お題
-	Odai string `json:"odai"`
 }
 
 // ルームID
@@ -272,15 +159,6 @@ type JoinRoomJSONBody JoinRoomRequest
 // CreateRoomJSONBody defines parameters for CreateRoom.
 type CreateRoomJSONBody CreateRoomRequest
 
-// WsJSONBody defines parameters for Ws.
-type WsJSONBody struct {
-	// Embedded fields due to inline allOf schema
-	Body interface{} `json:"body"`
-
-	// Websocketイベントのリスト
-	Type WsEvent `json:"type"`
-}
-
 // WsParams defines parameters for Ws.
 type WsParams struct {
 	// ユーザーUUID
@@ -292,6 +170,3 @@ type JoinRoomJSONRequestBody JoinRoomJSONBody
 
 // CreateRoomJSONRequestBody defines body for CreateRoom for application/json ContentType.
 type CreateRoomJSONRequestBody CreateRoomJSONBody
-
-// WsJSONRequestBody defines body for Ws for application/json ContentType.
-type WsJSONRequestBody WsJSONBody
