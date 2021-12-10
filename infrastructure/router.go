9ab695a3bf479d11df3a97bb9590f3c6dd05ec54
Ras96 <asymptote.k.k@gmail.com>
2021-12-10 19:51:59 +0900

    :recycle: Add skipper

--- infrastructure/router.go
+++ infrastructure/router.go
@@ -1,13 +1,19 @@
 package infrastructure
 
 import (
+	"strings"
+
 	"github.com/21hack02win/nascalay-backend/oapi"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func Setup(e *echo.Echo, baseEndpoint string) {
-	e.Use(middleware.Logger())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Skipper: func(c echo.Context) bool {
+			return strings.HasPrefix(c.Request().URL.String(), "/assets")
+		},
+	}))
 	e.Use(middleware.Recover())
 
 	s := injectServer()
