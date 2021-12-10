1b15a7a83905e8b7ee71a91e1b5da0a077821ff7
Ras96 <asymptote.k.k@gmail.com>
2021-12-10 19:55:57 +0900

    :shirt: Lint

--- util/random/shuffle.go
+++ util/random/shuffle.go
@@ -79,7 +79,7 @@ func SetupMemberRoles(g *model.Game, members []model.User) {
 				}
 				g.Odais[i].DrawerSeq = append(g.Odais[i].DrawerSeq, model.Drawer{
 					UserId: members[rect[i][j]].Id,
-					AreaId:  model.AreaId(ra[k]),
+					AreaId: model.AreaId(ra[k]),
 				})
 				k += 1
 			}
